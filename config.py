f86215a7b1ca97055344ccc3454846764b235577
Jakub Wilk <kub.wilk@gmail.com>
2025-11-30 19:40:26 +0000

    upadted config.py

--- config.py
+++ config.py
@@ -3,9 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Magia: Pobieramy dokładną ścieżkę do folderu, w którym leży ten plik
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///training.db'
+
+    # ZMIANA: Sklejamy ścieżkę folderu z nazwą pliku bazy
+    # Dzięki temu aplikacja zawsze trafi do tego samego pliku
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'training.db')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
     STRAVA_CLIENT_ID = os.environ.get('STRAVA_CLIENT_ID')
